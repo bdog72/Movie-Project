3d0d4a18a58333f8036037b6139fb4307e69f84f
Brian Bycynski <high5bri@hotmail.com>
2019-11-07 09:10:01 -0500

    finished lecture 175

---
+++ assets/scripts/app.js
@@ -0,0 +1,97 @@
+const addMovieModal = document.querySelector('#add-modal');
+const startAddMovieButton = document.querySelector('header button');
+const backdrop = document.querySelector('#backdrop');
+
+// const cancelModalButton = document.querySelector('#bozo');
+// const cancelModalButton = document.querySelector('.btn--passive');
+
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+
+const userInputs = addMovieModal.querySelectorAll('input');
+const entryTextSection = document.querySelector('#entry-text');
+
+const movies = [];
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = 'block';
+  } else {
+    entryTextSection.style.display = 'none';
+  }
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement('li');
+  newMovieElement.className = 'movie-element';
+  newMovieElement.innerHTML = `
+    <div class='movie-element__image'>
+      <img src='${imageUrl}' alt='${title}'
+    </div>
+    <div class='movie-element__info'>
+      <h2>${title}</h2>
+      <p>${rating}/5 stars</p>
+    </div>
+  `;
+  const listRoot = document.querySelector('#movie-list');
+  listRoot.appendChild(newMovieElement);
+};
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+};
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
+};
+
+const clearMovieInput = () => {
+  for (const userInput of userInputs) {
+    userInput.value = '';
+  }
+};
+
+const cancelAddMovieHandler = () => {
+  toggleMovieModal();
+  clearMovieInput();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === '' ||
+    imageUrlValue.trim() === '' ||
+    ratingValue.trim() === '' ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert('Please Enter Valid Values Between 1 and 5');
+    return;
+  }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue
+  };
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieInput();
+  // renderNewMovieElement(...newMovie);
+  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  updateUI();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener('click', addMovieHandler);
